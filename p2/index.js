0b856d2e7e8e521f7a0370f5ffb92dd918d25654
devSudhir <sudhirsamantaray33@gmail.com>
2023-11-30 05:23:35 +0530

    variable basics

---
+++ p2/index.js
@@ -0,0 +1,23 @@
+// variables decalration
+
+let banana = "banana";
+console.log(banana);
+
+//You cannot use the reserved keywords
+// let var = "var";
+// console.log(var);
+
+//A varibale don't have spaces in it, it shouldn't start with numbers, and special characters are not allowed other than _ and $.
+
+let name = "Sudhir";
+let whatDoYouWannaBecomeInYourLife = "A best human being";
+let gender = "Male";
+let linkedLink = "https://www.linkedin.com/in/devSudhir/";
+
+console.log(name);
+
+console.log(whatDoYouWannaBecomeInYourLife);
+
+console.log(gender);
+
+console.log(linkedLink);
