59a14e6da36fc2d7cd9f03f5ee0bb7e0d9aa5f22
devSudhir <sudhirsamantaray33@gmail.com>
2023-11-30 05:51:23 +0530

    arithmetic operators , boolean variable and falsy values

---
+++ p3/index.js
@@ -0,0 +1,98 @@
+//primitive datatypes
+
+let x = 10;
+let y = 20;
+let z = 30;
+
+console.log(typeof x);
+
+//basic maths
+console.log(x * 2);
+
+console.log(x / 2);
+
+console.log(x % 2);
+
+console.log(x ** 3);
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+console.log(x--);
+
+console.log(y++);
+
+console.log(x, y);
+
+//challenge
+
+//create a variable name firrtFavNumber and store favorite number.
+
+let firstFavNumber = 3;
+
+//create a variable name secondFavNumber and store favorite number.
+
+let secondFavNumber = 7;
+
+//Add firstfavNumber to secondFavNumber
+
+console.log(firstFavNumber + secondFavNumber);
+
+//subtract firstFavNumber from secondFavNumber
+
+console.log(firstFavNumber - secondFavNumber);
+
+//multiply firstFavNumber by secondFavNumber
+
+console.log(firstFavNumber * secondFavNumber);
+
+//divide firstFavNumber by secondFavNumber
+
+console.log(firstFavNumber / secondFavNumber);
+
+//mod firtFavNumber with secondFavNumber
+
+console.log(firstFavNumber % secondFavNumber);
+
+//check power of firstFavNumber with secondFavNUmber
+
+console.log(firstFavNumber ** secondFavNumber);
+
+//boolean variable
+
+let isHappy = true;
+
+console.log(isHappy);
+
+isHappy = false;
+
+//falsy values (null, Nan, 0, undefined, false, "", '', ``)
+
+//challenge - 2
+
+//create a variable neme isJsProgrammingLanguage and store true as a value
+
+let isJsProgrammingLanguage = true;
+
+console.log(isJsProgrammingLanguage);
+
+//create a variable neame isJsHard and store false as a value
+
+const isJsHard = false;
+
+console.log(isJsHard);
+
+//create a variable name favNumber and store your favourite number as a value
+
+const favNumber = 3;
+
+console.log(favNumber);
+
+//Add favNumber with value of undefined
+
+console.log(favNumber + undefined);
